12aaaa5b52770d6a8d084243864499c03e19a82c
Joshua Mengel <17039@cgs.act.edu.au>
2017-05-24 17:12:46 +1000

    Added the mine.py file for code cadets and modified the test.txt file to reflect the state of Git

---
+++ mine.py
@@ -0,0 +1,33 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+import time
+
+x = 100
+y = 100
+z = 100
+
+pos = mc.player.getTilePos()
+
+playerX = pos.x
+playerY = pos.y
+playerZ = pos.z
+
+time.sleep(4)
+
+#mc.player.setTilePos(x,y,z)
+
+blockType = 171, 14
+
+mc.setBlock(playerX,(playerY-1),playerZ, blockType)
+
+mc.setBlock(x,y,z, blockType)
+
+i = 0
+
+while i < 100:
+    #mc.setBlock(x,y,z, blockType)
+    #time.sleep(0.1)
+    #mc.player.setTilePos(x,y,z)
+    #y = y + 1
+    i = i + 1
